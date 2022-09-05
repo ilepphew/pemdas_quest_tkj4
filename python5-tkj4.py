2e553b49699f92716020d6ae3b882cc19010dfa5
ilepphew <112526154+ilepphew@users.noreply.github.com>
2022-09-05 08:26:06 +0700

    Add files via upload

---
+++ python5-tkj4.py
@@ -0,0 +1,23 @@
+# dollar ke rupiah
+
+print ("jumlah dollar ke rupiah adalah")
+dollar =int(input("dollar = "))
+
+rupiah = dollar * 14.897
+print ("rupiah=", rupiah)
+
+# uero ke yen
+
+print ("jumlah euro ke yen adalah")
+euro =int(input("euro = "))
+
+yen = euro * 139.16
+print ("yen=", yen)
+
+# won ke rupiah
+
+print ("jumlah won ke rupiah adalah")
+won =int(input("won = "))
+
+rupiah = won * 10.92
+print ("rupiah=", rupiah)
