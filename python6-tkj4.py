7022830851f1225bee58bf1e23c46d9cad5917fb
ilepphew <112526154+ilepphew@users.noreply.github.com>
2022-09-05 09:44:45 +0700

    Add files via upload

---
+++ python6-tkj4.py
@@ -0,0 +1,10 @@
+
+list_kosong = []
+list_buah = ["pisang" , "nanas", "melon" , "durian"]
+list_nilai = [80, 70, 90, 60]
+list_jawaban = [150, 33, 33, "presiden sukarno"]
+
+print ('list_kosong:', list_kosong)
+print ('list_buah:', list_buah)
+print ('list_nilai:', list_nilai)
+print ('list_jawaban',list_jawaban)
