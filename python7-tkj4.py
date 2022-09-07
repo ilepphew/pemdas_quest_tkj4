c05a641977f863cf6d646802cbb70c7026f68d3e
ilepphew <112526154+ilepphew@users.noreply.github.com>
2022-09-07 08:43:21 +0700

    Add files via upload

---
+++ python7-tkj4.py
@@ -0,0 +1,30 @@
+# mendata list
+
+list_kosong = []
+list_integer = [1, 2, 3, 10, 100, 250]
+list_string = ["nazla", "dewi", "lulu"]
+list_mix = [20, "arya", True, "bryan"]
+
+print("list kosong", list_kosong)
+print("list data integer", list_integer)
+print("list data string", list_string)
+print("list data mix", list_mix)
+
+print("data kedua list string:", list_string[0])
+
+print("data pertama list data integer adalah:", list_integer[0])
+print("data kedua list data integer adalah:", list_integer[1])
+print("data ketiga list data integer adalah:", list_integer[2])
+print("data keempat list data integer adalah:", list_integer[3])
+print("data kelima list data integer adalah:", list_integer[4])
+print("data keenam list data integer adalah:", list_integer[5])
+
+
+print("data pertama list data string adalah:", list_string[0])
+print("data kedua list data string adalah:", list_string[1])
+print("data ketiga list data string adalah:", list_string[2])
+
+print("data pertama list data mix adalah:", list_mix[0])
+print("data kedua list data mix adalah:", list_mix[1])
+print("data ketiga list data mix adalah:", list_mix[2])
+print("data keempat list data mix adalah:", list_mix[3])
