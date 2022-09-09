e9112606903c1c5dbc88a193553416ee852c6fe2
ilepphew <112526154+ilepphew@users.noreply.github.com>
2022-09-09 14:33:02 +0700

    Add files via upload

---
+++ Uprak_Nazla leviana putri_10TKJ4.py
@@ -0,0 +1,41 @@
+# buatlah sintaks menghitung volume bangun ruang
+
+
+# volume bangun balok (panjang * lebar * tinggi)
+
+panjang = 5
+lebar = 7
+tinggi = 10
+volume_B = panjang * lebar * tinggi
+print ("volume balok adalah", volume_B)
+
+# volume bangun kubus (sisi * sisi * sisi)
+
+sisi1 = 20
+sisi2 = 20
+sisi3 = 20
+volume_K = sisi1 * sisi2 * sisi3
+print ("volume kubus adalah", volume_K)
+
+# volume bangun limas {1/3 * luas alas * tinggi)
+
+luasalas = 30
+tinggi = 10
+volume_L = luasalas * tinggi / 3
+print ("volume limas adalah", volume_L)
+
+# volume bangun tabung (phi * r * r * t)
+
+phi = 22/7
+ruas = 21
+tinggi = 15
+volume_T = ruas * ruas * tinggi * phi
+print ("volume tabung adalah", volume_T)
+
+# buatlah sintaks yang menerjemahkan suhu celsius ke dalam suhu reaumur
+
+print ("jumlah celsius ke reamur adalah")
+celsius =int(input("celsius = "))
+
+reamur = celsius * 5
+print ("reamur=", reamur)
