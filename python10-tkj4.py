01a144319345aec9bf9de00deb3a3a2dd6a45089
ilepphew <112526154+ilepphew@users.noreply.github.com>
2022-10-10 09:47:09 +0700

    Add files via upload

---
+++ python10-tkj4.py
@@ -0,0 +1,19 @@
+# membuat tipe data koleksi
+
+koleksi_data_str = ["pisang", "mangga", "jambu", "semangka"]
+
+koleksi_data_int = [200, 300, 400, 500]
+
+koleksi_data_mix = ["rizky billar", 100, True, "reza arap"]
+
+print("koleksi data string:", koleksi_data_str)
+
+print("koleksi data integer:", koleksi_data_int)
+
+print("koleksi data campuran:", koleksi_data_mix)
+
+# buatlah 3 kumpulan data : nama hewan, nilai uts, nama teman sebangku  kalian
+
+koleksi_data_pribadi = ["burung hantu", 92, "lulu", "panda", 86, "dewi", "beruang", 96]
+
+print("koleksi data pribadi:", koleksi_data_pribadi)
