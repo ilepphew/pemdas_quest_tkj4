75a8141296e9596144b73d63cbd32b47d63535cc
ilepphew <112526154+ilepphew@users.noreply.github.com>
2022-10-18 09:41:47 +0700

    Add files via upload

---
+++ python11_tkj4.py
@@ -0,0 +1,21 @@
+#general 
+gender = ("laki-laki", "perempuan")
+
+#menggunakan fungsi tuple
+tuple_name = tuple(["jayabaya", "leborn james"])
+
+#tuple tunggal
+tuple_tunggal = ("albedo",)
+
+#gabungan
+gabungan = gender + tuple_name
+
+print(gabungan)
+
+hewan_darat = ("panda", "beruang", "kucing", "ular")
+hewan_udara = ("elang", "pelikan", "merpati", "gagak")
+hewan_laut = ("hiu", "paus", "ubur-ubur", "nemo")
+
+print(hewan_darat)
+print(hewan_udara)
+print(hewan_laut)
